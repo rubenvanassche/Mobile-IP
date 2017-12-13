1a823f76c2ea21cca05c1cf3873741e0972f7eb5
Ruben Van Assche <rubenvanassche@gmail.com>
2017-12-13 14:40:15 +0100

    Updated klaar lijst

--- elements/MobileIPNode.cc
+++ elements/MobileIPNode.cc
@@ -46,6 +46,7 @@ void MobileIPNode::run_timer(Timer *timer) {
 		}
 
 		// Decrease the lifetime of the current registration if there is one
+		// TODO: this is stilll quite a mess, should be cleaned up
 		if(this->connection.remainingLifetime > 0){
 			this->connection.remainingLifetime -= 1;
 
